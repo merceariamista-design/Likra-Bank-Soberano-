6799a9deb7b7b5ad5f6eb64f86be27dea0583b0b
merceariamista-design <merceariamista@gmail.com>
2026-01-20 20:51:31 -0300

    Update bancoCentral.js

--- bancoCentral.js
+++ bancoCentral.js
@@ -1,24 +1,12 @@
 let bancoCentral = {
     inflacao: 4,
     politica: "Neutra",
-    evento: "Equilíbrio global"
+    presidente: "Indicado"
 };
 
-function aplicarEvento(){
-    const eventos = [
-        {txt:"Crise global", inflacao:+0.7},
-        {txt:"Expansão mundial", inflacao:-0.4},
-        {txt:"Choque energético", inflacao:+0.5},
-        {txt:"Estabilidade prolongada", inflacao:-0.2}
-    ];
-
-    let e = eventos[Math.floor(Math.random()*eventos.length)];
-    bancoCentral.evento = e.txt;
-    bancoCentral.inflacao = Math.max(0,bancoCentral.inflacao+e.inflacao);
-
+function aplicarPolitica(){
+    bancoCentral.presidente = governoAtual.nome;
     bancoCentral.politica =
-        bancoCentral.inflacao>6?"Restritiva":
-        bancoCentral.inflacao<2?"Expansionista":"Neutra";
-
-    eventoAtual.innerText = bancoCentral.evento;
+        governoAtual.jurosBase > 2 ? "Restritiva" :
+        governoAtual.jurosBase < 1.5 ? "Expansionista" : "Neutra";
 }
