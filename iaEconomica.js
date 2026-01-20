0e16817284c179499926a97c7901793f8608db37
merceariamista-design <merceariamista@gmail.com>
2026-01-20 20:52:47 -0300

    Update iaEconomica.js

--- iaEconomica.js
+++ iaEconomica.js
@@ -1,20 +1,21 @@
 let ia = [
     {nome:"Fundo Atlas", capital:500000},
-    {nome:"Consórcio NovaEra", capital:800000},
-    {nome:"Grupo Ômega", capital:1200000}
+    {nome:"Grupo Ômega", capital:900000}
 ];
 
 function agirIA(){
     ia.forEach(i=>{
-        let acao = Math.random();
-        if(acao>0.6){
-            i.capital*=1.05;
-            jogo.precoAcao*=0.98;
-        }else if(acao<0.3){
-            i.capital*=0.97;
-            jogo.precoAcao*=1.03;
+        let fator = Math.random();
+        if(fator > 0.5){
+            i.capital *= 1.03;
+            jogo.precoAcao *= 0.99;
+        } else {
+            i.capital *= 0.97;
+            jogo.precoAcao *= 1.02;
         }
     });
 
-    iaStatus.innerHTML = ia.map(i=>`${i.nome}: ${i.capital.toFixed(0)} K$`).join("<br>");
+    iaStatus.innerHTML = ia
+        .map(i=>`${i.nome}: ${i.capital.toFixed(0)} K$`)
+        .join("<br>");
 }
