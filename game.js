d80ce979ac5eeed095f66c3e4b71ce0bb61f9110
merceariamista-design <merceariamista@gmail.com>
2026-01-20 21:07:24 -0300

    Update game.js

--- game.js
+++ game.js
@@ -15,7 +15,7 @@ function salvar(){
 
 function log(txt){
     jogo.historico.unshift(new Date().toLocaleString()+" - "+txt);
-    if(jogo.historico.length>150) jogo.historico.pop();
+    if(jogo.historico.length>200) jogo.historico.pop();
     salvar();
     render();
 }
@@ -30,59 +30,17 @@ function render(){
     renderCambio();
 }
 
-function emprestimo(){
-    let v=Number(valorEmprestimo.value);
-    let juros = bancoCentral.jurosDiario;
-    jogo.emprestimo+=v;
-    jogo.saldo+=v;
-    log("Empréstimo tomado");
-}
+function cicloSistema(){
+    aplicarPolitica();
+    agirIA();
+    agirMultiplayer();
+    validarConstituicao();
 
-function pagarEmprestimo(){
-    let total = jogo.emprestimo * (1 + bancoCentral.jurosDiario);
-    if(jogo.saldo>=total){
-        jogo.saldo-=total;
-        fundo+=total-jogo.emprestimo;
-        jogo.emprestimo=0;
-        log("Empréstimo quitado");
-    }
-}
+    jogo.precoAcao *= (1+(Math.random()*3-1.5)/100);
 
-function comprarImovel(){
-    if(jogo.saldo>=1000){
-        jogo.saldo-=1000;
-        jogo.imoveis++;
-        log("Imóvel comprado");
-    }
-}
-
-function receberAluguel(){
-    let ganho=jogo.imoveis*120*(1-bancoCentral.inflacao/100);
-    jogo.saldo+=ganho;
-    fundo+=ganho*0.0023;
-    log("Aluguel recebido");
-}
-
-function comprarAcao(){
-    if(jogo.saldo>=jogo.precoAcao){
-        jogo.saldo-=jogo.precoAcao;
-        jogo.investido+=jogo.precoAcao;
-        log("Ação comprada");
-    }
-}
-
-function venderAcao(){
-    if(jogo.investido>=jogo.precoAcao){
-        jogo.saldo+=jogo.precoAcao;
-        jogo.investido-=jogo.precoAcao;
-        log("Ação vendida");
-    }
-}
-
-setInterval(()=>{
-    jogo.precoAcao*=(1+(Math.random()*4-2)/100);
     salvar();
     render();
-},15000);
+}
 
+setInterval(cicloSistema,15000);
 render();
