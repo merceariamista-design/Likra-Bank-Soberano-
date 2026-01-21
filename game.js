0fcded2075e2d39a8a082761f46adc32fa53e0a0
merceariamista-design <merceariamista@gmail.com>
2026-01-20 21:19:50 -0300

    Update game.js

--- game.js
+++ game.js
@@ -1,49 +1,62 @@
-let jogo = JSON.parse(localStorage.getItem("likraBank")) || {
-    saldo:3000,
-    investido:0,
-    emprestimo:0,
-    imoveis:0,
-    precoAcao:100,
-    historico:[]
+let socket = new WebSocket("ws://localhost:8080");
+
+let jogo = {
+    saldo: 3000,
+    precoAcao: 100,
+    historico: []
 };
 
-let fundo = 7000000000;
+socket.onmessage = event => {
+    let msg = JSON.parse(event.data);
 
-function salvar(){
-    localStorage.setItem("likraBank", JSON.stringify(jogo));
+    if(msg.tipo === "sync"){
+        jogo.precoAcao = msg.estado.precoAcao;
+        bancoCentral.inflacao = msg.estado.inflacao;
+        render();
+    }
+};
+
+function enviarEstado(){
+    socket.send(JSON.stringify({
+        tipo: "update",
+        precoAcao: jogo.precoAcao,
+        inflacao: bancoCentral.inflacao
+    }));
 }
 
 function log(txt){
-    jogo.historico.unshift(new Date().toLocaleString()+" - "+txt);
-    if(jogo.historico.length>300) jogo.historico.pop();
-    salvar();
+    jogo.historico.unshift(new Date().toLocaleTimeString()+" - "+txt);
+    if(jogo.historico.length > 100) jogo.historico.pop();
     render();
 }
 
 function render(){
-    saldo.innerText=jogo.saldo.toFixed(2);
-    investido.innerText=jogo.investido.toFixed(2);
-    precoAcao.innerText=jogo.precoAcao.toFixed(2);
-    inflacao.innerText=bancoCentral.inflacao.toFixed(2);
-    governo.innerText=governoAtual.nome;
-    historico.innerHTML=jogo.historico.join("<br>");
+    saldo.innerText = jogo.saldo.toFixed(2);
+    precoAcao.innerText = jogo.precoAcao.toFixed(2);
+    inflacao.innerText = bancoCentral.inflacao.toFixed(2);
+    historico.innerHTML = jogo.historico.join("<br>");
     renderCambio();
 }
 
-function cicloFinal(){
-    aplicarPolitica();
-    agirIA();
-    agirMultiplayer();
-    operarBancos();
-    auditarSistema();
-    validarConstituicao();
-    sincronizar();
-
-    jogo.precoAcao *= (1+(Math.random()*2-1)/100);
-
-    salvar();
-    render();
+function comprarAcao(){
+    if(jogo.saldo >= jogo.precoAcao){
+        jogo.saldo -= jogo.precoAcao;
+        jogo.precoAcao *= 1.01;
+        log("Compra de ação");
+        enviarEstado();
+    }
 }
 
-setInterval(cicloFinal,12000);
+function venderAcao(){
+    jogo.saldo += jogo.precoAcao;
+    jogo.precoAcao *= 0.99;
+    log("Venda de ação");
+    enviarEstado();
+}
+
+setInterval(()=>{
+    jogo.precoAcao *= (1 + (Math.random()*2-1)/100);
+    enviarEstado();
+}, 10000);
+
 render();
