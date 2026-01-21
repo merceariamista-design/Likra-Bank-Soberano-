823f01388147abdc1e5955d9d09352dfa3da38ca
merceariamista-design <merceariamista@gmail.com>
2026-01-20 21:20:35 -0300

    Update mercado.js

--- mercado.js
+++ mercado.js
@@ -5,30 +5,21 @@ let moedas = {
 };
 
 function renderCambio(){
-    cambio.innerHTML="";
+    cambio.innerHTML = "";
     for(let m in moedas){
-        let div=document.createElement("div");
+        let div = document.createElement("div");
         div.innerHTML = `
             ${m.toUpperCase()} — ${moedas[m].toFixed(2)} K$
             <button onclick="comprarMoeda('${m}')">Comprar</button>
-            <button onclick="venderMoeda('${m}')">Vender</button>
         `;
         cambio.appendChild(div);
     }
 }
 
 function comprarMoeda(m){
-    let custo = moedas[m]*1.037;
-    if(jogo.saldo>=custo){
-        jogo.saldo-=custo;
-        fundo+=moedas[m]*0.037;
-        log("Compra de "+m);
+    let custo = moedas[m];
+    if(jogo.saldo >= custo){
+        jogo.saldo -= custo;
+        log("Compra de moeda " + m);
     }
-}
-
-function venderMoeda(m){
-    let ganho = moedas[m]*0.963;
-    jogo.saldo+=ganho;
-    fundo+=moedas[m]*0.037;
-    log("Venda de "+m);
 }
